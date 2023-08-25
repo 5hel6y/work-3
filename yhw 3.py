d9f5b7ab86fdf07bcd7331ff2ab57ad4573c1a28
5hel6y <89475726+5hel6y@users.noreply.github.com>
2023-08-25 15:51:33 +0300

    Add files via upload

---
+++ yhw 3.py
@@ -0,0 +1,6 @@
+string1 = 'Съешь ещё этих мягких французских булок ДА выпей же чаю'
+string2 = (string1.split())
+string3 = (str.upper(string2[3]))
+string4 = (str.lower(string2[6]))
+string5 = (string2[7])
+print(string2 , string3 , string4 , (string5[2]))
